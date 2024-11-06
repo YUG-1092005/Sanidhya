f18840c410329509f5ed627d08fb122dee7fec6a
Yug Trivedi <yugt5223@gmail.com>
2024-11-06 08:49:35 +0530

    Added mongoose expertVideoModel in CallServer

--- Servers/CallServer/SocketContext.js
+++ Servers/CallServer/SocketContext.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { ExpressPeerServer } = require("peer");
 const cors = require("cors");
-const expertVideoId = require("../models/expertVideoModel");
+const expertVideoId = require("./expertVideoModel");
 const connectDB = require("../config/db.cjs");
 require("dotenv").config({ path: __dirname + "/../.env" });
 
--- Servers/CallServer/expertVideoModel.js
+++ Servers/CallServer/expertVideoModel.js
@@ -1,4 +1,3 @@
-// models/Expert.js
 const mongoose = require("mongoose");
 
 const expertVideoSchema = new mongoose.Schema({
